c563355e5f870f744b58af06fc950546c5a18c5d
achaillot <anthony-34400@hotmail.fr>
2017-12-01 10:51:39 +0100

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TripsDisplayComponent } from './trips-display/trips-display.component';
 import { LoginComponent } from './login/login.component';
@@ -17,7 +17,8 @@ import { AuthentificationService, UserService } from './services/index';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   providers: [AuthentificationService, UserService],
   bootstrap: [AppComponent]
