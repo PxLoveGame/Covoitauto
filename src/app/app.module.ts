43fa67db1c990e226f54f36a22e283a6027c2ae8
achaillot <anthony-34400@hotmail.fr>
2017-12-01 10:21:03 +0100

    Ajout des composant login et register,  du dossiers services et service authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { TripsDisplayComponent } from './trips-display/trips-display.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthentificationComponent,
     TripsDisplayComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
