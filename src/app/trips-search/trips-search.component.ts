01b648833eafe87eee387be877aca6c4809a8a47
ThibautE <thibaut07600@gmail.com>
2017-12-08 12:25:36 +0100

    ajout fonction test valeur

--- src/app/trips-search/trips-search.component.ts
+++ src/app/trips-search/trips-search.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripsSearchComponent implements OnInit {
 
+	villeD : string;
+	villeA : string;
+	dateT : Date;
+	cpt : int = 0;
+
+	correspondTrips : Object[] = [];
+
+	//à retirer une fois la bdd fonctionnel
+	trips: Object[] = [
+    {"startCity":"Montpellier", "endCity":"Avignon", "date":"12/25/2017", "place":"0"},
+    {"startCity":"Montpellier", "endCity":"Paris", "date":"01/01/2018", "place":"2"},
+    {"startCity":"Nice", "endCity":"Lyon", "date":"12/20/2017", "place":"2"},
+    {"startCity":"Nice", "endCity":"Lyon", "date":"10/20/2017", "place":"1"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+	searchTrips(){
+  		for(var i = 0; i< this.trips.length; i++){
+			if(this.trips[i]['startCity'] === villeA && this.trips[i]['endCity'] === villeD && this.trips[i]['date'] === dateT && this.trips[i]['place']>0){
+				correspondTrips[i] = '{"startCity":' +'"this.villeA", "endCity":' +'"this.villeD", "date":' + '"this.dateT", "place":' + '"this.trips[i]['place']"}';
+				this.cpt++;
+			}
+		}
+  	}
 }
