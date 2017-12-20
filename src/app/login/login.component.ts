bd97a6f83d0e6128599cf2bcab1e5fb372ccfaab
ThibautE <thibaut07600@gmail.com>
2017-12-20 15:34:10 +0100

    modification css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,8 @@ import { AuthentificationService } from '../services/index';
  
 @Component({
     moduleId: module.id,
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['./login.component.css']
 })
  
 export class LoginComponent implements OnInit {
